881d81a153c9b61b720c5d969f2dc6d810f868b1
Jose Soares <josefsoares@gmail.com>
2021-02-07 16:17:43 -0500

    [-] Stuff that was pending

--- modes/+lsp.el
+++ modes/+lsp.el
@@ -5,8 +5,8 @@
 ;;
 
 (after! lsp-mode
-  (setq-default lsp-pyls-configuration-sources ["pylint" "flake8" "pycodestyle"])
-  (setq lsp-pyls-plugins-pylint-enabled t)
+  ;; (setq-default lsp-pyls-configuration-sources ["pylint" "flake8" "pycodestyle"])
+  ;; (setq lsp-pyls-plugins-pylint-enabled t)
 
   ;; angular lsp
   (setq lsp-clients-angular-language-server-command
@@ -16,4 +16,9 @@
           ,(concat (getenv "HOME") "/.nvm/versions/node/v12.0.0/lib/node_modules")
           "--tsProbeLocations"
           ,(concat (getenv "HOME") "/.nvm/versions/node/v12.0.0/lib/node_modules")
-          "--stdio")))
+          "--stdio"))
+
+  ;; (defvar lsp-docker-client-packages
+  ;;   '(lsp-css lsp-clients lsp-bash lsp-pyls lsp-html ))
+
+  )

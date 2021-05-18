848f92dd03daf617c4d104c1fc1637a47f237a15
Jose Soares <jose@linux.com>
2021-05-18 14:08:37 -0400

    [-] New pylsp (fork) / remove fci again

--- modes/+lsp.el
+++ modes/+lsp.el
@@ -8,6 +8,13 @@
   ;; (setq-default lsp-pyls-configuration-sources ["pylint" "flake8" "pycodestyle"])
   ;; (setq lsp-pyls-plugins-pylint-enabled t)
 
+  ;; Use the forked version of pyls
+  ;; https://github.com/python-lsp/python-lsp-server
+  (setq lsp-pyls-server-command "pylsp")
+
+  ;; https://emacs-lsp.github.io/lsp-mode/page/performance/
+  (setq read-process-output-max (* 1024 1024)) ;; 1mb
+
   ;; angular lsp
   (setq lsp-clients-angular-language-server-command
         `("node"

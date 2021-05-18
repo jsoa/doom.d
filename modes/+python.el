847e71c0fde440319f22eb2b1cc2e98cd30ea0d5
Jose Soares <josefsoares@gmail.com>
2021-05-18 14:08:37 -0400

    [-] New pylsp (fork) / remove fci again

--- modes/+python.el
+++ modes/+python.el
@@ -4,5 +4,8 @@
 ;; Python mode
 ;;
 (add-hook! python-mode
-  (fci-mode)
+  ;; The main reason this was removed is because it seems like the line
+  ;; shifts auto-complete options to the right of the line (just for the
+  ;; first item in the option list)
+  ;; (fci-mode)
   )

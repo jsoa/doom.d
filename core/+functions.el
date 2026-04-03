747c258c4a67d0abf615ad239c5e0c8d8e8c6a6b
jsoa <jose@linux.com>
2026-04-03 14:14:52 -0400

    [-] More tweaks
    - Removed unused functions (yea all of them)
    - Add fci module that was in general

--- core/+functions.el
+++ core/+functions.el
@@ -5,16 +5,3 @@
 ;;
 
 
-;; https://emacs.stackexchange.com/questions/13080/reloading-directory-local-variables
-(defun jsoa/reload-dir-locals-for-current-buffer ()
-  "reload dir locals for the current buffer"
-  (interactive)
-  (let ((enable-local-variables :all))
-    (hack-dir-local-variables-non-file-buffer)))
-
-
-(defun jsoa/file-info ()
-  (interactive)
-  (let ((dired-listing-switches "-alh"))
-    (dired-other-window buffer-file-name)))
-

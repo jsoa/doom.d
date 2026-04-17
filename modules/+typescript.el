19ad18f7c6b0a336a1376e799dfb9c389343890a
jsoa <jose@linux.com>
2026-04-17 13:28:05 -0400

    [-] Cleaning up
    - maximize window when opening emacs
    - moved some code from lsp to html module
    - moved some other code form lsp to typescript module

--- modules/+typescript.el
+++ modules/+typescript.el
@@ -7,19 +7,6 @@
 
 
 (after! typescript-mode
-  (setq-default typescript-indent-level 2)
-  ;; (flycheck-add-next-checker 'lsp-ui 'typescript-tslint)
-  ;; (map! :after dap-mode
-  ;;       :localleader
-  ;;       :map typescript-mode-map
-  ;;       :prefix ("d" . "debugger")
-  ;;       :desc "start"              "s" #'dap-debug
-  ;;       :desc "hydra"              "h" #'dap-hydra
-  ;;       :desc "add breakpoint"     "a" #'dap-breakpoint-add
-  ;;       :desc "remove breakpoint"  "d" #'dap-breakpoint-delete
-  ;;       :desc "toggle breakpoint"  "t" #'dap-breakpoint-toggle
-  ;;       :desc "show locals"        "l" #'dap-ui-locals
-  ;;       :desc "show session"       "S" #'dap-ui-sessions
-  ;;       :desc "show breakpoints"   "B" #'dap-ui-breakpoints
-  ;;       )
-  )
+  (setq-default typescript-indent-level 2))
+
+(add-hook 'typescript-ts-mode-hook #'lsp-deferred)

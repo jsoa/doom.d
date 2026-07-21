f1213ff25c05e3e1bba9a972751a0e60f9952a6e
Jose Soares <jose@linux.com>
2026-07-20 20:57:38 -0400

    [-] Allow private dir to be anywhere as long as its a sibling of this repo

--- modules/+private.el
+++ modules/+private.el
@@ -1,18 +1,14 @@
 ;;; modules/+private.el -*- lexical-binding: t; -*-
 
-
-(defcustom js/private-doom-directory
-  (expand-file-name "~/.doom-private/")
-  "Directory containing the optional private Doom configuration."
-  :type 'directory
-  :group 'doom)
+(defconst js/private-doom-directory
+  (expand-file-name
+   "doom-private/"
+   (file-name-directory
+    (directory-file-name doom-user-dir)))
+  "Directory containing the optional private Doom configuration.")
 
 (defun js/load-private-doom-config ()
-  "Load the private Doom configuration when it is available.
-
-The private repository is expected to contain a machine-local
-`config.el' entry point. If the repository or entry point does not
-exist, do nothing."
+  "Load the private Doom configuration when available."
   (let ((private-config
          (expand-file-name
           "config.el"
